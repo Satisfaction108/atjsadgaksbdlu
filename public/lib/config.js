4e5585269a82cf086f8865173cf52d4c9cad337a
Satisfaction108 <167273050+Satisfaction108@users.noreply.github.com>
2024-07-21 17:44:54 -0700

    Update config.js

--- public/lib/config.js
+++ public/lib/config.js
@@ -21,12 +21,11 @@ function resetTarget() {
 
 global.tips = [[ // You can edit this!
         "Tip: You can view and edit your keybinds in the options menu.",
-        "Tip: You can play on mobile by just going to arras.io on your phone!"
+        "Tip: You can play on mobile by just going to tarras.io on your phone!"
     ], [
         "Tip: You can have the shield and health bar be separated by going to the options menu.",
-        "Tip: If arras is having a low frame rate, you can try enabling low graphics in the options menu.",
+        "Tip: If tarras is having a low frame rate, you can try enabling low graphics in the options menu.",
         "Tip: You can make traps rounded with the classic trap setting in the options menu.",
-        "Tip: You can create your own private server with the template in the link on the options menu.",
         "Tip: You can create your own theme with the custom theme makerin the link on the options menu."
     ], [
         "Teaming in FFA or FFA Maze is frowned upon, but when taken to the extremes, you can be punished.",
