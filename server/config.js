8628d177170340947d920cd4601cca31401ebb4e
Satisfaction108 <167273050+Satisfaction108@users.noreply.github.com>
2024-07-27 18:52:59 -0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,10 +3,10 @@ module.exports = {
 
     // Game server domain.
     // If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
-    host: "localhost:26301",
+    host: "localhost:26300",
 
     // Which port to run the web server on.
-    port: 26301,
+    port: 26300,
 
     // How often to update the list of the entities that players can see.
     // Has effects of when entities are activated.
